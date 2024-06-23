2e0d8a4347e62e8d5daf8db2f9068e3655c9a13c
Alex Mota <alexmota.work@gmail.com>
2024-06-22 21:23:17 -0300

    configurando para https

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://final-web-server-production.up.railway.app",
+  baseURL: "https://final-web-server-production.up.railway.app",
   headers: {
     'Content-Type': 'application/json',
   }
