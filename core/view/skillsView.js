3f100a8c8fabf2c67a6bd1d97072405b05772317
Alex Mota <alexmota.work@gmail.com>
2025-02-09 17:50:14 -0300

    feat: Updating visualization to skill views for show experience in year e not in percentual

--- core/view/skillsView.js
+++ core/view/skillsView.js
@@ -1,19 +1,20 @@
 const skills = [
-  { name: "Java", value: 95 },
-  { name: "Spring Boot", value: 70 },
-  { name: "C#", value: 90 },
-  { name: ".NET", value: 70 },
-  { name: "SQL", value: 75 },
-  { name: "Typescript", value: 80 },
-  { name: "Node JS", value: 80 },
-  { name: "ReactJS", value: 94 },
-  { name: "Python", value: 90 },
-  { name: "HTML", value: 95 },
-  { name: "CSS", value: 90 },
-  { name: "JavaScript", value: 85 },
+  { name: "Java", value: 4 },
+  { name: "Spring Boot", value: 2 },
+  { name: "C#", value: 3 },
+  { name: ".NET", value: 2 },
+  { name: "SQL", value: 3 },
+  { name: "Typescript", value: 3 },
+  { name: "Node JS", value: 3 },
+  { name: "ReactJS", value: 3 },
+  { name: "HTML", value: 4. },
+  { name: "CSS", value: 4 },
+  { name: "JavaScript", value: 4 },
 ];
 
+const MAX_YEARS = 6;
 const skillsContainer = document.querySelector(".skills-content");
+
 const column1 = document.createElement("div");
 column1.classList.add("col-lg-6");
 column1.setAttribute("data-aos", "fade-up");
@@ -26,10 +27,15 @@ column2.setAttribute("data-aos-delay", "100");
 skills.forEach((skill, index) => {
   const skillElement = document.createElement("div");
   skillElement.classList.add("progress");
+
+  const widthPercentage = (skill.value / MAX_YEARS) * 100;
+
   skillElement.innerHTML = `
-    <span class="skill">${skill.name} <i class="val">${skill.value}%</i></span>
+    <span class="skill">${skill.name} <i class="val">${skill.value} anos</i></span>
     <div class="progress-bar-wrap">
-      <div class="progress-bar" role="progressbar" aria-valuenow="${skill.value}" aria-valuemin="0" aria-valuemax="100" data-value="${skill.value}" style="width: 0;"></div>
+      <div class="progress-bar" role="progressbar" aria-valuenow="${skill.value}" 
+           aria-valuemin="0" aria-valuemax="${MAX_YEARS}" data-value="${widthPercentage}" 
+           style="width: 0;"></div>
     </div>
   `;
 
@@ -43,14 +49,13 @@ skills.forEach((skill, index) => {
 skillsContainer.appendChild(column1);
 skillsContainer.appendChild(column2);
 
-// Função para animar as barras
 function animateProgressBars() {
   const progressBars = document.querySelectorAll(".progress-bar");
 
   progressBars.forEach((bar) => {
-    let value = bar.getAttribute("data-value");
+    let value = parseFloat(bar.getAttribute("data-value"));
     let width = 0;
-    let speed = value / 50; // Define a velocidade da animação
+    let speed = value / 50;
 
     let interval = setInterval(() => {
       if (width >= value) {
@@ -59,11 +64,10 @@ function animateProgressBars() {
         width++;
         bar.style.width = width + "%";
       }
-    }, 15); // Intervalo de atualização da animação
+    }, 15);
   });
 }
 
-// Ativa a animação quando a seção entra na tela
 function onScrollAnimate() {
   const section = document.querySelector(".skills-content");
   const sectionPosition = section.getBoundingClientRect().top;
